bcd450a9f79cad124440e69dc922f2fbd5b33075
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-05-13 17:59:49 -0500

    added spotify-this-song default

--- liri.js
+++ liri.js
@@ -1,49 +1,105 @@
+// require APIs
+const Spotify = require("node-spotify-api");
 // read and set any environment variables with the dotenv package
 require("dotenv").config();
 // import the keys.js file and store it in a variable
 const keys = require("./keys.js");
 // access keys information
-const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
 
 // make it so liri.js can take in one of the following commands:
-console.log(process.argv);
+// console.log(process.argv);
 let command = process.argv[2];
-console.log(command);
 let input = process.argv[3];
-console.log(input);
 
-
-    // concert-this
-    // spotify-this-song
-    // movie-this
-    // do-what-it-says
 
 // concert-this
-    // search Bands in Town Artist Events API for an artist
-        // render name of venue
-        // render venue location
-        // render date of the event (use moment to format as MM/DD/YYYY)
-
 // spotify-this-song
-    // utilize the node-spotify-api package to retrieve song information
-        // render Artist
-        // render song name
-        // render a preview link of the song from Spotify
-        // render the album the song is on
-    // if no song is provided, default to "The Sign" by Ace of Base
+// movie-this
+// do-what-it-says
 
-// movie-this 
+// concert-this
+if (command == "concert-this") {
+    console.log("I am searching Bands in Town API")
+    // search Bands in Town Artist Events API for an artist
+    // render name of venue
+    // render venue location
+    // render date of the event (use moment to format as MM/DD/YYYY)
+}
+// spotify-this-song
+if (command == "spotify-this-song") {
+    if (input == undefined) {
+        spotify
+            .request('https://api.spotify.com/v1/tracks/3DYVWvPh3kGwPasp7yjahc')
+            .then(function (data) {
+                // console.log(data);
+                //   console.log(JSON.stringify(data, null, 2));
+                // render Artist
+                let songArtist = data.album.artists[0].name
+                // render song name
+                let songName = data.name
+                // render a preview link of the song from Spotify
+                let songPreview = data.album.preview_url
+                // render the album the song is on
+                let songAlbum = data.album.name
+                console.log("Title: ", songName)
+                console.log("Artist/Band:", songArtist)
+                console.log("Album: ", songAlbum)
+                if (songPreview == null) {
+                    console.log("I'm sorry, there is no song preview available")
+                } else {
+                    console.log(songPreview)
+                }
+            })
+            .catch(function (err) {
+                console.error('Error occurred: ' + err);
+            });
+
+
+    } else {
+        console.log("I am searching Spotify API")
+        // utilize the node-spotify-api package to retrieve song information
+        spotify.search({ type: 'track', query: input }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            //   console.log(JSON.stringify(data, null, 2));
+            // render Artist
+            let songArtist = data.tracks.items[0].artists[0].name
+            // render song name
+            // render a preview link of the song from Spotify
+            let songPreview = data.tracks.preview_url
+            // render the album the song is on
+            let songAlbum = data.tracks.items[0].album.name
+            console.log("Title: ", input)
+            console.log("Artist/Band:", songArtist)
+            console.log("Album: ", songAlbum)
+            if (songPreview == null) {
+                console.log("I'm sorry, there is no song preview available")
+            } else {
+                console.log(songPreview)
+            }
+
+        });
+    }
+}
+
+// if no song is provided, default to "The Sign" by Ace of Base
+
+// movie-this
+if (command == "movie-this") {
+    console.log("I am searching OMDB API")
     // use axios package to retrive data from OMDB API
-        // render title of movie
-        // render year movie came out
-        // render IMDB rating
-        // render Rotten Tomatoes rating
-        // render country where movie was produced
-        // render language of the movie
-        // render plot of the movie
-        // render actors in the movie
+    // render title of movie
+    // render year movie came out
+    // render IMDB rating
+    // render Rotten Tomatoes rating
+    // render country where movie was produced
+    // render language of the movie
+    // render plot of the movie
+    // render actors in the movie
     // if no song is provided, default to "Mr. Nobody"
+}
 
 // do-what-it-says
     //using fs Node package, take text inside of random.txt and use it to call one of LIRI's commands
