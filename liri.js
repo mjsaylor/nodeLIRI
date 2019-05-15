c84c9288f7df995a27d4c19bf9bd575d62bda034
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-05-14 21:39:02 -0500

    activated movie-this command including default

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 const Axios = require("axios")
 const moment = require("moment")
+const fs = require("fs")
 // require APIs
 const Spotify = require("node-spotify-api");
 // read and set any environment variables with the dotenv package
@@ -21,27 +22,27 @@ let input = process.argv[3];
 
 // concert-this
 if (command == "concert-this") {
-    console.log("I am searching Bands in Town API")
+    // console.log("I am searching Bands in Town API")
     // search Bands in Town Artist Events API for an artist
     Axios
         .get(`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp"`)
-        .then(function (response){
+        .then(function (response) {
             // console.log(response.data)
             // console.log(JSON.stringify(response.data, null, 2))
             // render name of venue
-    let venue = response.data[0].venue.name
-    // render venue location
-    let venueCity = response.data[0].venue.city
-    let venueState = response.data[0].venue.region
-    // render date of the event (use moment to format as MM/DD/YYYY)
-    let eventDate = moment(response.data[0].datetime, "YYYY-MM-DD, h:mm:ss").format("MM/DD/YYYY")
-    console.log(`
+            let venue = response.data[0].venue.name
+            // render venue location
+            let venueCity = response.data[0].venue.city
+            let venueState = response.data[0].venue.region
+            // render date of the event (use moment to format as MM/DD/YYYY)
+            let eventDate = moment(response.data[0].datetime, "YYYY-MM-DD, h:mm:ss").format("MM/DD/YYYY")
+            console.log(`
         Venue: ${venue}
         Location: ${venueCity}, ${venueState}
         Date: ${eventDate}
         `)
         })
-    
+
 }
 // spotify-this-song
 if (command == "spotify-this-song") {
@@ -60,11 +61,11 @@ if (command == "spotify-this-song") {
                 let songPreview = data.album.preview_url
                 // render the album the song is on
                 let songAlbum = data.album.name
-                console.log(
-                `Title: ${songName}
-                Artist/Band: ${songArtist}
-                Album: ${songAlbum}`
-                )
+                console.log(`
+                    Title: ${songName}
+                    Artist/Band: ${songArtist}
+                    Album: ${songAlbum}
+                    `)
                 if (songPreview == null) {
                     console.log("I'm sorry, there is no song preview available")
                 } else {
@@ -74,10 +75,8 @@ if (command == "spotify-this-song") {
             .catch(function (err) {
                 console.error('Error occurred: ' + err);
             });
-
-
     } else {
-        console.log("I am searching Spotify API")
+        // console.log("I am searching Spotify API")
         // utilize the node-spotify-api package to retrieve song information
         spotify.search({ type: 'track', query: input }, function (err, data) {
             if (err) {
@@ -91,9 +90,11 @@ if (command == "spotify-this-song") {
             let songPreview = data.tracks.preview_url
             // render the album the song is on
             let songAlbum = data.tracks.items[0].album.name
-            console.log("Title: ", input)
-            console.log("Artist/Band:", songArtist)
-            console.log("Album: ", songAlbum)
+            console.log(`
+                Title: ${input}
+                Artist/Band: ${songArtist}
+                Album: ${songAlbum}
+                `)
             if (songPreview == null) {
                 console.log("I'm sorry, there is no song preview available")
             } else {
@@ -108,21 +109,54 @@ if (command == "spotify-this-song") {
 
 // movie-this
 if (command == "movie-this") {
-    console.log("I am searching OMDB API")
-    // use axios package to retrive data from OMDB API
-    // render title of movie
-    // render year movie came out
-    // render IMDB rating
-    // render Rotten Tomatoes rating
-    // render country where movie was produced
-    // render language of the movie
-    // render plot of the movie
-    // render actors in the movie
+    // console.log("I am searching OMDB API")
     // if no song is provided, default to "Mr. Nobody"
+    if (input == undefined){
+        input = "Mr. Nobody"
+    }
+    // use axios package to retrive data from OMDB API
+    Axios
+        .get(`http://www.omdbapi.com/?apikey=trilogy&t=${input}`)
+        .then(function (response) {
+            // console.log(JSON.stringify(response.data, null, 2));
+            // render year movie came out
+            let movieYear = response.data.Year
+            // render IMDB rating
+            let imdbRating = response.data.Ratings[0].Value
+            // render Rotten Tomatoes rating
+            let rtRating = response.data.Ratings[1].Value
+            // render country where movie was produced
+            let movieCountry = response.data.Country
+            // render language of the movie
+            let movieLanguage = response.data.Language
+            // render plot of the movie
+            let moviePlot = response.data.Plot
+            // render actors in the movie
+            let movieActors = response.data.Actors
+            console.log(`
+            Title: ${input}
+            Year Released: ${movieYear}
+            IMDB Rating: ${imdbRating}
+            Rotten Tomatoes Rating: ${rtRating}
+            Country produced in: ${movieCountry}
+            Language: ${movieLanguage}
+            Plot: ${moviePlot}
+            Actors: ${movieActors}
+            `)
+          
+        });
 }
 
 // do-what-it-says
     //using fs Node package, take text inside of random.txt and use it to call one of LIRI's commands
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+
+        
+    })
     // run spotify-this-song for "I want it That way"
 
 
